e8c10a1df34f2bde1becca056c29ef75b75f1c4e
Mahesh1408-pro <maheshofficial735@gmail.com>
2025-06-27 18:48:22 +0000

    added some changes in conversion

--- basics/conversion.js
+++ basics/conversion.js
@@ -46,17 +46,18 @@ let str1 = "hello"
 let str2 = " hitesh"
 
 let str3 = str1 + str2
-// console.log(str3);
+console.log(str3); //hello hitesh
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2);  //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+console.log("2"+"2"); //22
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true); //1
+// console.log(+""); //0
 
 let num1, num2, num3
 
@@ -64,7 +65,7 @@ num1 = num2 = num3 = 2 + 2
 
 let gameCounter = 100
 ++gameCounter;
-console.log(gameCounter);
+console.log(gameCounter);//101
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
