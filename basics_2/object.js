2a4533119fb07179a8e4cddcfc07eac6655d41d2
Mahesh1408-pro <maheshofficial735@gmail.com>
2025-07-03 14:49:40 +0000

    objects start

---
+++ basics_2/object.js
@@ -0,0 +1,42 @@
+// singleton
+// constructor se singleton banega
+//literals se singleton nahi banega
+
+// Object.create  //constructor method
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);  ///important
+}
+
+console.log(JsUser.greeting);   //undefined
+console.log(JsUser.greeting()); //Hello JS user
+console.log(JsUser.greetingTwo());
